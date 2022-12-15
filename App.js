9ef227641411a24143ac33683bafb618e42e4a2c
이한슬 <>
2022-12-15 16:52:06 +0900

    이한슬 문구 추가

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ export default function App() {
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <Text>hanseulllll</Text>
+        <Text>이한슬</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ test.js
@@ -0,0 +1,5 @@
+import {Text, View} from "react-native";
+
+<View>
+    <Text>이한슬</Text>
+</View>
